d3ba542ecce3105cd2624aef50390fe4d69a5ea3
augusto.dev@proton.me <augusto.dev@proton.me>
2023-11-12 01:21:08 -0300

    feat: :sparkles: removendo expanded

--- lib/pages/v2design/chooseService/choose_service_page.dart
+++ lib/pages/v2design/chooseService/choose_service_page.dart
@@ -71,62 +71,58 @@ class ChooseServicePage extends StatelessWidget {
                   );
                 })),
           ),
-          Expanded(
-            child: SizedBox.expand(
-              child: DraggableScrollableSheet(
-                maxChildSize: 0.9,
-                minChildSize: 0.1,
-                snap: true,
-                snapSizes: const [0.1, 0.5, 0.9],
-                controller: resumeSericeScrollController,
-                builder:
-                    (BuildContext context, ScrollController scrollController) {
-                  return Container(
-                    padding: const EdgeInsets.symmetric(horizontal: 16),
-                    decoration: const BoxDecoration(
-                      color: Colors.black,
-                      borderRadius: BorderRadiusDirectional.only(
-                        topEnd: Radius.circular(22),
-                        topStart: Radius.circular(22)
-                      )
-                    ),
-                    child: SingleChildScrollView(
-                      controller: scrollController,
-                      child: Column(
-                        children: [
-                          Container(
-                            margin: const EdgeInsets.only(top: 20),
-                            width: 70,
-                            height: 5,
-                            decoration: BoxDecoration(
-                              color: Colors.grey,
-                              borderRadius: BorderRadius.circular(5),
-                            ),
-                          ),
-                          const Align(
-                            alignment: Alignment.centerLeft,
-                            child: TextWidget(
-                              "Seu pedido",
-                              customFontsize: 16,
-                              isFontWeight: true,
-                              color: Colors.white,
-                            ),
-                          ),
-                          ListView.builder(
-                            shrinkWrap: true,
-                            physics: const NeverScrollableScrollPhysics(),
-                            itemCount: 25,
-                            itemBuilder: (BuildContext context, int index) {
-                              return ListTile(title: Text('Item $index'));
-                            },
-                          ),
-                        ],
+          DraggableScrollableSheet(
+            maxChildSize: 0.9,
+            minChildSize: 0.1,
+            snap: true,
+            snapSizes: const [0.1, 0.5, 0.9],
+            controller: resumeSericeScrollController,
+            builder:
+                (BuildContext context, ScrollController scrollController) {
+              return Container(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                decoration: const BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadiusDirectional.only(
+                    topEnd: Radius.circular(22),
+                    topStart: Radius.circular(22)
+                  )
+                ),
+                child: SingleChildScrollView(
+                  controller: scrollController,
+                  child: Column(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.only(top: 20),
+                        width: 70,
+                        height: 5,
+                        decoration: BoxDecoration(
+                          color: Colors.grey,
+                          borderRadius: BorderRadius.circular(5),
+                        ),
                       ),
-                    ),
-                  );
-                },
-              ),
-            ),
+                      const Align(
+                        alignment: Alignment.centerLeft,
+                        child: TextWidget(
+                          "Seu pedido",
+                          customFontsize: 16,
+                          isFontWeight: true,
+                          color: Colors.white,
+                        ),
+                      ),
+                      ListView.builder(
+                        shrinkWrap: true,
+                        physics: const NeverScrollableScrollPhysics(),
+                        itemCount: 25,
+                        itemBuilder: (BuildContext context, int index) {
+                          return ListTile(title: Text('Item $index'));
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
           ),
         ],
       ),
