6278d3779a07b2e985173a8db8b69571847fbb11
augusto.dev@proton.me <augusto.dev@proton.me>
2023-11-13 13:16:50 -0300

    feat: :sparkles: melhora no design

--- lib/pages/v2design/professionals/professionals_page.dart
+++ lib/pages/v2design/professionals/professionals_page.dart
@@ -1,5 +1,3 @@
-import 'package:cabelin_app/widgets/layout_widget.dart';
-import 'package:cabelin_app/widgets/list_widget.dart';
 import 'package:cabelin_app/widgets/text_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
@@ -9,97 +7,117 @@ class ProfessionalsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return LayoutWidget(
-      child: Column(
-        children: [
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              const TextWidget(
-                "Escolha um profissional",
-                isFontWeight: true,
-                customFontsize: 16,
-              ),
-              IconButton(
-                icon: const Icon(
-                  Icons.close_rounded,
-                  color: Colors.grey,
-                ),
-                onPressed: () {
-                context.pop();
-              })
-            ],
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+        body: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+          decoration: const BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(16),
+              topRight: Radius.circular(16)
+            )
           ),
-          GridView.count(
-            shrinkWrap: true,
-            crossAxisCount: 2,
-            physics: const NeverScrollableScrollPhysics(),
-            mainAxisSpacing: 20,
-            crossAxisSpacing: 20,
-            children: List.generate(6, (index) {
-              return index == 0 
-                ? Container(
-                  padding: const EdgeInsets.all(16),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(16),
-                    border: Border.all(
-                      width: 2,
-                      color: Colors.grey
+          child: Column(
+            children: [
+              Container(
+                margin: const EdgeInsets.only(bottom: 16),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const TextWidget(
+                      "Escolha um profissional",
+                      isFontWeight: true,
+                      customFontsize: 16,
                     ),
-                  ),
-                  child: const Center(
-                    child:  Column(
-                      children: [
-                        Icon(Icons.transfer_within_a_station),
-                        TextWidget(
-                          "Qualquer profissional",
-                          isFontWeight: true,
-                          customFontsize: 12,
+                    GestureDetector(
+                      onTap: () =>context.pop(),
+                      child: Container(
+                        padding: const EdgeInsets.all(8),
+                        decoration: BoxDecoration(
+                          color: Colors.grey.withOpacity(0.3),
+                          borderRadius: const BorderRadius.all(Radius.circular(50))
                         ),
-                        TextWidget(
-                          "Faça o agendamento \n com qualquer profissional",
-                          customFontsize: 12,
-                        )
-                      ],
-                    ),
-                  ),
-                )
-                : GestureDetector(
-                  onTap: () => context.push("/v2/choose_service"),
-                  child: Container(
-                  padding: const EdgeInsets.all(16),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(16),
-                    border: Border.all(
-                      width: 2,
-                      color: Colors.grey
-                    ),
-                  ),
-                  child: Column(
-                    children: [
-                      Container(
-                        width: 70,
-                        height: 70,
-                        decoration: const BoxDecoration(
-                          color: Colors.blueAccent,
-                          borderRadius: BorderRadius.all(Radius.circular(10))
+                        child: Icon(
+                          Icons.close_rounded,
+                          color: Colors.black.withOpacity(0.5),
                         ),
                       ),
-                      const TextWidget(
-                        "Maria Silva",
-                        isFontWeight: true,
-                        customFontsize: 16,
-                        margin: EdgeInsets.only(top: 16),
-                      )
-                    ],
-                  ),
-                              ),
-                );
-            }),
+                    ),
+                  ],
+                ),
+              ),
+              GridView.count(
+                shrinkWrap: true,
+                crossAxisCount: 2,
+                physics: const NeverScrollableScrollPhysics(),
+                mainAxisSpacing: 20,
+                crossAxisSpacing: 20,
+                children: List.generate(6, (index) {
+                  return index == 0 
+                    ? Container(
+                      padding: const EdgeInsets.all(16),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(16),
+                        border: Border.all(
+                          width: 2,
+                          color: Colors.grey
+                        ),
+                      ),
+                      child: const Center(
+                        child:  Column(
+                          children: [
+                            Icon(Icons.transfer_within_a_station),
+                            TextWidget(
+                              "Qualquer profissional",
+                              isFontWeight: true,
+                              customFontsize: 12,
+                            ),
+                            TextWidget(
+                              "Faça o agendamento \n com qualquer profissional",
+                              customFontsize: 12,
+                            )
+                          ],
+                        ),
+                      ),
+                    )
+                    : GestureDetector(
+                      onTap: () => context.push("/v2/choose_service"),
+                      child: Container(
+                      padding: const EdgeInsets.all(16),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(16),
+                        border: Border.all(
+                          width: 2,
+                          color: Colors.grey
+                        ),
+                      ),
+                      child: Column(
+                        children: [
+                          Container(
+                            width: 70,
+                            height: 70,
+                            decoration: const BoxDecoration(
+                              color: Colors.blueAccent,
+                              borderRadius: BorderRadius.all(Radius.circular(10))
+                            ),
+                          ),
+                          const TextWidget(
+                            "Maria Silva",
+                            isFontWeight: true,
+                            customFontsize: 16,
+                            margin: EdgeInsets.only(top: 16),
+                          )
+                        ],
+                      ),
+                                  ),
+                    );
+                }),
+              ),
+            ],
           ),
-        ],
-      )
+        ),
     ); 
   }
 }
